29134a17e642654019a05f93e0bae94aa1d4e341
xuzh <xzh7868@qq.com>
2017-07-25 15:32:31 +0800

    add mongodb and readme

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 city = '广州'
 keyword = 'python'
+MONGO_URL = 'localhost'
+MONGO_NAME = 'lagou'
--- spider.py
+++ spider.py
@@ -1,6 +1,6 @@
 from getPage import get_index_data, get_job_data, get_company_data
 from parsePage import parse_index_data, parse_job_data, parse_company_data
-
+from save_mongo import save_to_mongo
 
 if __name__ == '__main__':
     desurl = 'https://www.lagou.com/jobs/positionAjax.json?'
@@ -8,4 +8,4 @@ if __name__ == '__main__':
     for item in parse_index_data(data):
         job_data = get_job_data(item['positionId'])
         item['job'] = parse_job_data(job_data)
-        print(item)
+        save_to_mongo(item)
